069ad8098727da3ca6a8269a7e8773abd60add7a
J.Parkes <james@ancroft.eu>
2018-08-03 18:29:37 +0100

    Euler 33

--- Pycharm Python Code/Euler Problems/Euler 33.py
+++ Pycharm Python Code/Euler Problems/Euler 33.py
@@ -1,50 +1,49 @@
 from fractions import Fraction
 
+def simplifyFraction (n,d):
+    fract = Fraction(n,d)
+    simFract = str(fract)
+    simFract = simFract.split("/")
+    return simFract
+
 fractions = []
 
 for n in range (10,100):
-    print("*************************************************************************************************************")
-
-    print(n)
-    print("")
-
     for d in range(10,100):
 
-        numer = n
-        denom = d
+        print(n)
 
+        #Splitting the numerator and denominator.
         nSplit = [int(z) for z in str(n)]
         dSplit = [int(s) for s in str(d)]
 
+        if not nSplit.__contains__(0) and not dSplit.__contains__(0):
 
-        for n in nSplit:
-            if n in dSplit and n != 0:
-                dSplit.remove(n)
-                nSplit.remove(n)
+            #Removing common values in numerator and denominator.
+            for n in nSplit:
+                if n in dSplit and n != 0:
+                    dSplit.remove(n)
+                    nSplit.remove(n)
 
-        if len(nSplit) == 0:
-            nSplit.append(1)
+            #Place a 1 in the fraction where there is no number.
+            if len(nSplit) == 0:
+                nSplit.append(1)
 
-        if len(dSplit) == 0:
-            nSplit.append(1)
+            if len(dSplit) == 0:
+                nSplit.append(1)
 
+            #Join the split numer and deno to form simplified numbers.
+            simNumer = ''.join(str(v) for v in nSplit)
+            simDenom = ''.join(str(v) for v in dSplit)
 
-        simNumer = ''.join(str(v) for v in nSplit)
-        simDenom = ''.join(str(v) for v in dSplit)
+            #Casts the joined numbers to ints
+            simNumer = int(simNumer)
+            simDenom = int(simDenom)
 
-        simNumer = int(simNumer)
-        simDenom = int(simDenom)
+            simplFract = simplifyFraction(n,d)
+            #print(simplFract)
 
-        fract = str(Fraction(numer, denom)).split("/")
-
-        print(str(n) + "/" + str(d))
-        print(str(simNumer) + "/" + str(simDenom))
-        print(fract)
-        print("")
-
-        if(len(fract) == 2):
-            if int(fract[0]) == simNumer and int(fract[1]) == simDenom:
-               fractions.append(fract)
+            if simNumer == simplFract[0] and simDenom == simplFract[1]:
+                fractions.append(str(n," / ",d))
 
 print(fractions)
-#hello james
