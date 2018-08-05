aa6aa07c4ffa718663ee6bf818321d0dd0d77444
J.Parkes <james@ancroft.eu>
2018-08-05 16:26:18 +0100

    Euler 36 Finish

--- Pycharm Python Code/Euler Problems/Euler 36.py
+++ Pycharm Python Code/Euler Problems/Euler 36.py
@@ -1,25 +1,27 @@
-def DenToBin(d):
-    columns = [128,64,32,16,8,4,2,1]
-    bin = [0,0,0,0,0,0,0,0]
-
-    denary = d
-
-    for i in range(0,columns.__len__()):
-        fac = divmod(denary,columns[i])[0]
-        bin[i] = fac
-        denary -= fac * columns[i]
-
-    return  bin
-
-def split_list(a_list):
-    half = int(len(a_list)/2)
-    return a_list[:half], a_list[half:]
 
 def isPalindrome(s):
-    split = []
-    split = split_list(s)
 
-    if split[0] == list(reversed(split[1])):
+    s = [int(n) for n in str(s)]
+
+    if s == list(reversed(s)):
         return True
     else:
         return False
+
+
+complete = False
+
+i = 0
+palindromes = []
+
+while not complete:
+    if isPalindrome(bin(i)[2:]):
+        if isPalindrome(i):
+            palindromes.append(i)
+
+    if i == 1000000:
+        complete = True
+
+    i+=1
+print(palindromes)
+print(sum(palindromes))
