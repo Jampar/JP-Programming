880cdea534e8969636465206db8cb08858ae7ab1
J.Parkes <james@ancroft.eu>
2018-08-10 22:23:15 +0100

    Euler 75 Finsished!

--- Pycharm Python Code/Euler Problems/Euler 75.py
+++ Pycharm Python Code/Euler Problems/Euler 75.py
@@ -1,31 +1,46 @@
 import numpy as np
-import  time
+import time
 from collections import Counter
 
 first = time.time()
 targetLength = 10000000
 
-def generate_primitives(limit):
 
+def oppParity(m, n):
+    if (m % 2 == 0 and not n % 2 == 0) or (n % 2 == 0 and not m % 2 == 0):
+        return True
+    else:
+        return False
+
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+
+def generate_primitives(limit):
     L = 0
     m = 1
     primitives = []
     i = 0
     while L <= limit:
 
-        for n in range(1,m):
+        for n in range(1, m):
+            if oppParity(m, n) and gcd(m, n) == 1:
 
-            a = m*m - n*n
-            b = 2*m*n
-            c = m*m + n*n
-            L = a+b+c
+                a = m * m - n * n
+                b = 2 * m * n
+                c = m * m + n * n
+                L = a + b + c
 
-            if L <= 1500000 and not a % 2 == 0:
-                primitives.append(sorted([a,b,c]))
+                if L <= 1500000 and not a % 2 == 0:
+                    primitives.append(sorted([a, b, c]))
 
         m += 1
 
     return primitives
+
 
 def generate_nonPrimitives(primitives):
     triples = []
@@ -35,8 +50,6 @@ def generate_nonPrimitives(primitives):
 
     for p in range(0, primitives.__len__()):
 
-        # Adding the primitive to the triple list.
-        triples.append(primitives[p])
 
         if primitives[p][2] % 2 == 0:
             divTrip = [int(i * 0.5) for i in primitives[p]]
@@ -56,14 +69,38 @@ def generate_nonPrimitives(primitives):
             multi += 1
     return mTriples + dTriples
 
-primitives = generate_primitives(targetLength)
-non_Primitives = generate_nonPrimitives(primitives)
 
-triples = primitives + non_Primitives
-triples = np.array(triples)
-#triples = np.unique(triples, axis=0)
+def generate_triples():
+    primitives = generate_primitives(targetLength)
+    non_Primitives = generate_nonPrimitives(primitives)
 
-print(triples)
+    triples = primitives + non_Primitives
+    triples = np.array(triples)
+
+    return triples
+
+
+def perimeter_list(triple):
+    perimeters = []
+    for t in triple:
+        perimeters.append(sum(t))
+
+    return perimeters
+
+
+triples = generate_triples()
+perimeters = perimeter_list(triples)
+
+C = Counter(perimeters)
+
+perimeters = [[k, ] * v for k, v in C.items()]
+
+uniPerim = []
+for P in perimeters:
+    if len(P)== 1 :
+        uniPerim.append(P[0])
+
+print(len(uniPerim))
 
 last = time.time()
-print("Time Taken:", last - first,"secs")
+print("Time Taken:", last - first, "secs")
