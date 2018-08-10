58b47f1d0488afbb85ba88e2d973165a7a59ce85
J.Parkes <james@ancroft.eu>
2018-08-10 21:25:42 +0100

    Euler 75

--- Pycharm Python Code/Euler Problems/Euler 75.py
+++ Pycharm Python Code/Euler Problems/Euler 75.py
@@ -1,62 +1,69 @@
 import numpy as np
 import  time
+from collections import Counter
 
 first = time.time()
+targetLength = 10000000
 
-L = 0
-m = 1
-targetLength = 1500000
+def generate_primitives(limit):
 
-primitives = []
+    L = 0
+    m = 1
+    primitives = []
+    i = 0
+    while L <= limit:
 
-while L <= targetLength:
+        for n in range(1,m):
 
-    for n in range(1,m):
+            a = m*m - n*n
+            b = 2*m*n
+            c = m*m + n*n
+            L = a+b+c
 
-        a = m*m - n*n
-        b = 2*m*n
-        c = m*m + n*n
-        L = a+b+c
+            if L <= 1500000 and not a % 2 == 0:
+                primitives.append(sorted([a,b,c]))
 
-        if L <= targetLength:
-            primitives.append(sorted([a,b,c]))
+        m += 1
 
-    m += 1
+    return primitives
 
-triples = []
-divTrip = []
-dTriples = []
-mTriples = []
+def generate_nonPrimitives(primitives):
+    triples = []
+    divTrip = []
+    dTriples = []
+    mTriples = []
 
+    for p in range(0, primitives.__len__()):
 
-for p in range(0,primitives.__len__()):
+        # Adding the primitive to the triple list.
+        triples.append(primitives[p])
 
-    #Adding the primitive to the triple list.
-    triples.append(primitives[p])
+        if primitives[p][2] % 2 == 0:
+            divTrip = [int(i * 0.5) for i in primitives[p]]
+            dTriples.append(sorted(divTrip))
 
-    if primitives[p][2] % 2 == 0:
-        divTrip = [int(i * 0.5) for i in primitives[p]]
-        dTriples.append(sorted(divTrip))
+        multi = 2
+        pLength = 0
 
-    multi = 2
-    pLength = 0
+        while pLength <= 1500000:
 
-    while pLength <= targetLength:
+            mulTrip = [i * multi for i in primitives[p]]
+            pLength = sum(mulTrip)
 
-        mulTrip = [i * multi for i in primitives[p]]
-        pLength = sum(mulTrip)
+            if pLength <= 1500000:
+                mTriples.append(mulTrip)
 
-        if pLength <= targetLength:
-            mTriples.append(mulTrip)
+            multi += 1
+    return mTriples + dTriples
 
-        multi += 1
+primitives = generate_primitives(targetLength)
+non_Primitives = generate_nonPrimitives(primitives)
 
-
-triples = triples + dTriples + mTriples
+triples = primitives + non_Primitives
 triples = np.array(triples)
-triples = np.unique(triples, axis=0).tolist()
+#triples = np.unique(triples, axis=0)
 
-print(len(triples))
+print(triples)
 
 last = time.time()
 print("Time Taken:", last - first,"secs")
